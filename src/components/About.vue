ed7de40ee4c1a76a9f6b89c347db8cb5d535558e
Zak <zakaryholliman@gmail.com>
2022-04-06 14:24:13 -0400

    Made dummy routes I think

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h1>Gameing</h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+
+const name: string = "asd";
+const pass: string = "gfasd";
+
+const name2 = ref("");
+const pass2 = ref("");
+
+function submit(){
+  name2.value = name;
+  pass2.value = pass;
+}
+
+</script>
+
+<template>
+  <div>
+    <p>Username : {{ name2 }}</p>
+    <p>Password : {{ pass2 }}</p>
+  </div>
+
+  <div>
+    <p>Username</p>
+    <input v-model="name">
+  </div>
+  
+  <div>
+    <p>Password</p>
+    <input v-model="pass">
+  </div>
+
+  <div>
+    <button @click="submit">Submit</button>
+  </div>
+
+  
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
