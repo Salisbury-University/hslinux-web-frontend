c1b812a20513d28b2abc7a8efc7f3c341fa79c1d
braiden <braiden@braiden-ThinkPad-E15-Gen-2.(none)>
2022-04-07 16:05:49 -0400

    Changed colors and created route to login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import About from "../components/About.vue";
+import HomeLayout from "../layouts/HomeLayout.vue"
 </script>
 
 <template>
-  <a href="/login">Login</a> |
-  <a href="/about">About</a>
-</template> -->
+  <HomeLayout/>
+</template>
