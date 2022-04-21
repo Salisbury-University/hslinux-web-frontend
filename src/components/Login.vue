6d66bba9fa5812cc0acb14d4387125a05e59a7b7
Zak <zakaryholliman@gmail.com>
2022-04-21 14:30:04 -0400

    Starred password and cleaned up Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,6 +4,10 @@ import { ref } from "vue"
 const textName = ref("");
 const textPassword = ref("");
 
+function submit(){
+  // This needs to send a request to the backend to verify the username and password given and get the token
+}
+
 // Login is going to have to take the username and passwords that are submitted by the user, and send them
 // to the backend as a request to see if it is valid and what kind of access the user has to the wiki (student, 
 // faculty, admin, etc...)
