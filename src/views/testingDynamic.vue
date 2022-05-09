0f8ba5c0927d6f0192aeffaf198e8ab23378706e
braiden <braiden@braiden-ThinkPad-E15-Gen-2.(none)>
2022-05-09 19:20:32 -0400

    location for marked files

--- src/views/testingDynamic.vue
+++ src/views/testingDynamic.vue
@@ -1,14 +1,16 @@
 <script lang="ts">
-import req from "../scripts/scripts"
+import req from "../scripts/scripts";
+
+import { ref } from "vue";
+
+const markdown = "<div class='text' id='marked'>hello world</div>";
 </script>
 
 <template>
-    <div>
-        testingDynamic {{ $route.params.id }}
-        <button @click=req>Click Me</button>
-    </div>
+  <div>
+    testingDynamic {{ $route.params.id }}
+    <button @click="req">Click Me</button>
+  </div>
 
-    <div id="pageContents">
-        
-    </div>
+  <div v-html="markdown"></div>
 </template>
