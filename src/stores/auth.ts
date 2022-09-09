549670e8781dc5e35ad50584efa4a27d8d9f6ffb
Zakary Holliman <zakaryholliman@gmail.com>
2022-09-09 12:24:10 -0400

    Made some comments

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,21 +2,30 @@ import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core";
 import http from "../http";
 
+/** This is a pinia store that keeps track of all persistent data */
+
 export const useAuthStore = defineStore("useAuthStore", {
   state: () => {
     return {
       nonpersistence: {
         uid: "cxarausa",
         password: "testing",
-        baseURL: "http://localhost:3005",
+        baseURL: "http://localhost:3005", // The url that the site will try to send POST and GET requests to
       },
       persistence: useLocalStorage("auth", {
-        token: "",
-        showLogin: true,
+        token: "", // Stored token for a logged in user
+        showLogin: true, // Used as reference as to whether the "login" input field should be shown on the login page
       }),
     };
   },
   actions: {
+    /** Login function : Send a POST request to the specified api path,
+     *    attempting to use the uid and password from the input field.
+     *    On successful login, catches and stores a unique token and
+     *      disables the "login" input field variable.
+     *    In event of an error, logs the appropriate error message that was
+     *      caught out to the console
+     */
     login() {
       http()
         .post("/api/v1/auth/login", {
@@ -26,12 +35,13 @@ export const useAuthStore = defineStore("useAuthStore", {
         .then((response) => {
           this.persistence.token = response.data.token;
           this.persistence.showLogin = false;
-          // Clean up debug
         })
         .catch(function (error) {
           console.log(error);
         });
     },
+    /** Lougout Function : Clears the user's token, and enables the "login"
+     *    input field variable */
     logout() {
       this.persistence.token = "";
       this.persistence.showLogin = true;
