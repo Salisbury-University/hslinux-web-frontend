09b5c300cb7d07db9e682907950f2529171b1cf1
Braiden Ryan <bryan4@hs101-3.hslinux>
2022-09-09 11:56:44 -0400

    worked on connecting front end to back end

--- src/scripts/scripts.ts
+++ src/scripts/scripts.ts
@@ -3,7 +3,7 @@ import http from "../../http";
 import { marked } from "marked";
 
 const req = async () => {
-  axios
+  http()
     .get("/api/v1/doc/:id")
     .then((res) => {
       console.log(marked.parse(res.data.content));
