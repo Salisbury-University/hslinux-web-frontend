f6719aa6553c6f039e9bac2dd366235a4f2171a5
Zakary Holliman <zakaryholliman@gmail.com>
2022-09-14 12:06:33 -0400

    .

--- src/http.ts
+++ src/http.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
-// import { useAuthStore } from "../stores/auth.ts";
-
-// const storer = uuseAuth
 
 export default function () {
   return axios.create({
     baseURL: "http://localhost:3005", // TODO - Trying to figure out how to access this from the pinia store
-    // baseURL: env.VITE_BASEURL,
     timeout: 8000,
     headers: {
       Authorization: "",
