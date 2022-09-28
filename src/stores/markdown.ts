a89f89dd45c2f70a57f17a39cbacb71bf0e2f423
Braiden Ryan <bryan4@hs101-3.hslinux>
2022-09-28 12:37:39 -0400

    creating markdown stores

---
+++ src/stores/markdown.ts
@@ -0,0 +1,51 @@
+import { defineStore } from "pinia";
+import { useStorage } from "@vueuse/core";
+import http from "../../http";
+import { info } from "console";
+
+export const usePageStore = defineStore("page", {
+  state: () => {
+    return {
+      content: [],
+    };
+  },
+  getters: {
+    /**
+     * get markdown content
+     *
+     * @param {any} state all state information
+     * @returns {string} content state
+     */
+
+    getContent(state: any): string {
+      return state.content;
+    },
+  },
+
+  actions: {
+    /**
+     * set content
+     *
+     * @param {string} newContent content of markdown
+     */
+
+    setContent(newContent: string): void {
+      this.content = newContent;
+    },
+
+    setPage(): void {
+      //two http calls first is to get amount of documents second is to get individual content
+
+      //array to hold page content
+      for (let i = 0; i < info.length; i++) {
+        http()
+          .get("/api/v1/doc/".concat(info[i]))
+          .then((response) => {
+            const metadata = response.data.metadata;
+
+            this.content = metadata.content;
+          });
+      }
+    },
+  },
+});
