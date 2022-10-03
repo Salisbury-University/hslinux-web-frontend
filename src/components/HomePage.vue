a76181f3b3c48d09f4f3b22d0fcf595c11940176
Braiden Ryan <bryan4@hs101-3.hslinux>
2022-10-03 12:25:42 -0400

    minor changes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,3 @@
-<script setup lang="ts">
-import { defineComponent, ref } from "vue";
-import markDownToHtml from "../scripts/scripts";
+<script setup lang="ts"></script>
 
-export default defineComponent({
-  el: "#Contents",
-  data() {
-    return {
-      markDown: "<p>Home Page</p>",
-    };
-  },
-});
-</script>
-
-<template>
-  <div>
-    <button @click="changeMarkDown()">Change markDown</button>
-  </div>
-  <br /><br />
-  <div id="Contents" v-html="markDown"></div>
-</template>
+<template></template>
--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { usePageStore } from "../stores/markdown";
+import { ref } from "vue";
+
+const usePage = usePageStore();
+usePage.setPage();
+
+const pageSize = usePage.title.length; //amount of documents
 </script>
 
 <template>
-  <div></div>
+  <div v-for="i in pageSize" :key="i">
+    {{ usePage.title[i] }}
+  </div>
 </template>
