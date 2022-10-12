9fc9af7cb8cdab1f8014fdbc9cc51c8089b00b0a
Zakary Holliman <zakaryholliman@gmail.com>
2022-10-12 11:42:42 -0400

    Preference page

--- src/components/Preferences.vue
+++ src/components/Preferences.vue
@@ -13,6 +13,8 @@ function sleep() {
   });
 }
 
+// TODO - When logging out, clear the persistent storage so that we go back to light mode
+
 async function buffer() {
   // Temporary for now maybe? We have to have a tiny buffer window between the v-model update and refereshing the page, or else it'll refresh using
   // the old value
@@ -23,9 +25,7 @@ onUpdated(() => {
   buffer();
   console.log("Test");
 
-  // TODO - Right now, this is included because when we update the dark mode we want to reflect the change immediately
-  // without having to refresh the page, but if we can somehow stealth-refresh the page then we can get rid of this and
-  // just have it refresh on the HomeLayout when it mounts like normal
+  $q.dark.set(store.persistence.darkMode);
 });
 </script>
 
